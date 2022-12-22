4585cc79ef08279005be495288f8bc094a8db8e1
Egor Zeziulko <egor.zezyulko@mail.ru>
2022-12-22 08:33:32 +0300

    Added via GitHub Desktop

--- Kollok2/src/Main.java
+++ Kollok2/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Man man = new Man(new Sleeping());
+        System.out.println(man.getState());
         man.changeState(new DrinkingSomeBeer());
         System.out.println(man.getState());
         System.out.println(man.getPreviousState());
